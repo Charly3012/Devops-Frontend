056353a4a1d042e19e72801bb7c5c096ba3a9074
Charly3012 <carlocoronadosilva975@gmail.com>
2025-05-21 12:25:30 -0600

    feat: Create Auth module
    - Create Auth module
    - Create components LoginComponent and RegisterComponent
    - Add Auth-routing module
    - Set up routes for Auth module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthLayoutComponent } from './core/layout/auth-layout/auth-layout.component';
 import { MainComponent } from './core/layout/main/main.component';
 import { HomeComponent } from './modules/home/pages/home/home.component';
 
 const routes: Routes = [
 
+
+  {
+    path: 'auth',
+    component: AuthLayoutComponent,
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
+  },
   {
     path: 'menu',
     component: MainComponent,
