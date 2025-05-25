1b30b0a60909584f080d20831585b78678a95107
Charly3012 <carlocoronadosilva975@gmail.com>
2025-05-25 16:13:25 -0600

    feat: Login page
    - Add Easter egg to login page
    - Validators to login form
    - Add methods to AuthService (setToken, getToken, logOut)
    - Redirect to home page after login
    - Redirect to login page after register
    - Add Models to login

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { loginRequest, loginResponse } from '../models/login.model';
 import { registerReponse, registerRequest } from '../models/register.model';
 
 @Injectable({
@@ -10,17 +13,42 @@ import { registerReponse, registerRequest } from '../models/register.model';
 export class AuthService {
 
   constructor(
-    private httpClient: HttpClient
+    private httpClient: HttpClient,
+    private toastService: ToastrService,
+    private router: Router
   ) { }
 
   private apiUrl: string = environment.apiUrl;
 
-  public login(){
-
-  }
-
   public register(data: registerRequest): Observable<registerReponse>{
     return this.httpClient.post<registerReponse>(`${this.apiUrl}/admin`, data);
+  }
+
+  public login(data: loginRequest): Observable<loginResponse>{
+    return this.httpClient.post<loginResponse>(`${this.apiUrl}/login`, data);
+  }
+
+  setToken(token:string){
+    sessionStorage.setItem('token', token);
+  }
+
+  getToken():string | void {
+    const token = sessionStorage.getItem('token');
+
+    if(token){
+      return token;
+    }
+
+    this.toastService.error('Intente iniciar sesión nuevamente','Error con su sesión');
+    setTimeout(() => {
+        this.logOut();
+      }, 2500);
+  }
+
+  logOut(){
+    this.toastService.info("Cerrando sesión...");
+    sessionStorage.removeItem('token');
+    this.router.navigate(['/auth/login']);
   }
 
 }
